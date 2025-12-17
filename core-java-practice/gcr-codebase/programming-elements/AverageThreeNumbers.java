cc0386f41be04057368f1b2826289cbede4e4ed4
Khushi Rathore <khushirathore02032003@gmail.com>
2025-12-17 16:52:24 +0530

    [Khushi] I added my average code and miles code

---
+++ core-java-practice/gcr-codebase/programming-elements/AverageThreeNumbers.java
@@ -0,0 +1,15 @@
+package leetcodeBase;
+import java.util.*;
+public class AverageThreeNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int num3=sc.nextInt();
+        int average=(num1+num2+num3)/3;
+        System.out.println(average);
+	}
+
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/KilometersToMiles.java
@@ -0,0 +1,14 @@
+package leetcodeBase;
+import java.util.*;
+public class KilometersToMiles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc=new Scanner(System.in);
+        int kilometers=sc.nextInt();
+        int miles=(int)( kilometers*0.621371);
+        System.out.println(miles);
+        		
+	}
+
+}

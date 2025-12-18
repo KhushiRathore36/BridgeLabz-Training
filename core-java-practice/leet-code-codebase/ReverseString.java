64fed5ca135f4aebf6b5f9ccd94df1a0d6077d5b
KhushiRathore36 <157281481+KhushiRathore36@users.noreply.github.com>
2025-12-18 11:35:34 +0530

    Update ReverseString.java

--- core-java-practice/leet-code-codebase/ReverseString.java
+++ core-java-practice/leet-code-codebase/ReverseString.java
@@ -1,10 +1,21 @@
 package leetcodeBase;
-
+import java.util.*;
 public class ReverseString {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		System.out.println(reverseWords(s));
 	}
-
+    public static String reverseWords(String s) {
+        s=s.trim();
+        String arr[]=s.split("\\s+");
+        String ans="";
+        for(int i=arr.length-1;i>0;i--){
+            ans=ans+arr[i]+" ";
+        }
+        ans=ans+arr[0];
+        return ans;
+    }
 }

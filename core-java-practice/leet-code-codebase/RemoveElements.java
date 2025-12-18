6682601716317cd59c03f05ca668060419e855c9
KhushiRathore36 <157281481+KhushiRathore36@users.noreply.github.com>
2025-12-18 11:32:35 +0530

    Update RemoveElements.java

--- core-java-practice/leet-code-codebase/RemoveElements.java
+++ core-java-practice/leet-code-codebase/RemoveElements.java
@@ -1,10 +1,26 @@
 package leetcodeBase;
-
+import java.util.*;
 public class RemoveElements {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        Scannner sc=new Scanner(System.in);
+		int val=sc.nextInt();
+		int length=sc.nextInt();
+		int nums[]=new int[length];
+		for(int i=0;i<length;i++){
+			nums[i]=sc.nextInt();
+		}
+		System.out.println(removeElement(nums,val));
 	}
-
+    public static int removeElement(int[] nums, int val) {
+        int n=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[n]=nums[i];
+                n++;
+            }
+        }
+        return n;
+    }
 }

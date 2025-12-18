3040d3e0431860c4f5456be678f4b02fc8c70fcb
KhushiRathore36 <157281481+KhushiRathore36@users.noreply.github.com>
2025-12-18 11:29:15 +0530

    Update PlaindromeNumber.java

--- core-java-practice/leet-code-codebase/PlaindromeNumber.java
+++ core-java-practice/leet-code-codebase/PlaindromeNumber.java
@@ -1,10 +1,27 @@
 package leetcodeBase;
-
+import java.util.*;
 public class PlaindromeNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		System.out.println(isPalindrome(x));
 	}
-
+    public static boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        String s=x+"";
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }

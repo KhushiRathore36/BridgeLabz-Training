b9db9727dce52a51b1008a3244be0b3ce4ac7677
KhushiRathore36 <157281481+KhushiRathore36@users.noreply.github.com>
2025-12-19 10:40:02 +0530

    Update AverageMarksInPCM.java

--- core-java-practice/gcr-codebase/programming-elements/AverageMarksInPCM.java
+++ core-java-practice/gcr-codebase/programming-elements/AverageMarksInPCM.java
@@ -4,10 +4,10 @@ public class AverageMarksInPCM {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        int Maths_Marks=94;
-        int Physics_Marks=95;
-        int Chemistry_Marks=96;
-        int average=(Maths_Marks+Physics_Marks+Chemistry_Marks)/3;
+        int mathsMarks=94;
+        int physicsMarks=95;
+        int chemistryMarks=96;
+        int average=(mathsMarks+physicsMarks+chemistryMarks)/3;
         System.out.println("average percentage in PCM Is: "+average);
 	}
 
